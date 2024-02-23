af5f80e7040c005c12eae18149c6c24fea2b5d8f
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-02-23 13:10:30 -0800

    Style table

--- app/ui/dashboard/table-row.tsx
+++ app/ui/dashboard/table-row.tsx
@@ -1,11 +1,21 @@
 export default function Row() {
   return (
     <tr className="hover:bg-gray-50 active:bg-emerald-100">
-      <td className="border-collapse border border-slate-300 px-8 py-4">1</td>
-      <td className="border-collapse border border-slate-300 px-8 py-4">2</td>
-      <td className="border-collapse border border-slate-300 px-8 py-4">3</td>
-      <td className="border-collapse border border-slate-300 px-8 py-4">4</td>
-      <td className="border-collapse border border-slate-300 px-8 py-4">5</td>
+      <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
+        1
+      </td>
+      <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
+        2
+      </td>
+      <td className="border-collapse border border-slate-300 px-2 py-2 md:x-8 md:py-4">
+        3
+      </td>
+      <td className="border-collapse border border-slate-300 px-2 py-2 md:x-8 md:py-4">
+        4
+      </td>
+      <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
+        5
+      </td>
     </tr>
   );
 }
--- app/ui/dashboard/table.tsx
+++ app/ui/dashboard/table.tsx
@@ -2,22 +2,22 @@ import Row from "./table-row";
 
 export default function Table() {
   return (
-    <table className="table-fixed border-collapse border border-slate-400 shadow-lg">
+    <table className="table-fixed border-collapse border border-slate-400 shadow-lg w-full">
       <thead className=" bg-gray-100">
         <tr>
-          <th className="border-collapse border border-slate-300 px-8 py-4">
+          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
             Company
           </th>
-          <th className="border-collapse border border-slate-300 px-8 py-4">
+          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
             Position
           </th>
-          <th className="border-collapse border border-slate-300 px-8 py-4">
+          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
             Status
           </th>
-          <th className="border-collapse border border-slate-300 px-8 py-4">
+          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
             Date
           </th>
-          <th className="border-collapse border border-slate-300 px-8 py-4">
+          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
             Note
           </th>
         </tr>
