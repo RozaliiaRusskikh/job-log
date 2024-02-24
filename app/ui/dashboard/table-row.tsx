0c6807ace5c42779a2f26e64967a7116596da27e
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-02-23 21:39:54 -0800

    Add style to statuses

--- app/ui/dashboard/table-row.tsx
+++ app/ui/dashboard/table-row.tsx
@@ -1,23 +1,33 @@
 import { ApplicationProp } from "@/app/lib/definitions";
+import clsx from "clsx";
 
 const Row: React.FC<{ data: ApplicationProp }> = ({ data }) => {
   const { company, position, status, date, note } = data;
   return (
     <tr className="hover:bg-gray-50 active:bg-emerald-100">
       <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
-        ${company}
+        {company}
       </td>
       <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
-        ${position}
+        {position}
       </td>
       <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
-        ${status}
+        <p
+          className={clsx("rounded-full w-fit px-2 py-1", {
+            "bg-gray-200": status === "applied",
+            "bg-rose-500": status === "rejected",
+            "bg-yellow-500": status === "interviewing",
+            "bg-emerald-500": status === "offer",
+          })}
+        >
+          {status}
+        </p>
       </td>
       <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
-        ${date}
+        {date}
       </td>
       <td className="border-collapse border border-slate-300 px-2 py-2 md:px-8 md:py-4">
-        ${note}
+        {note}
       </td>
     </tr>
   );
