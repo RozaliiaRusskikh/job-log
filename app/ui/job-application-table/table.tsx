f84319a3d67ab40fc1af49fec7db8b82259c6ac0
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-02-28 11:26:21 -0800

    Add chevrons

--- app/ui/job-application-table/table.tsx
+++ app/ui/job-application-table/table.tsx
@@ -1,5 +1,7 @@
 import Row from "./table-row";
 import { ApplicationProp } from "@/app/lib/definitions";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { ChevronUpIcon } from "@heroicons/react/24/outline";
 
 const Table: React.FC<{ applications: ApplicationProp[] }> = ({
   applications,
@@ -8,17 +10,33 @@ const Table: React.FC<{ applications: ApplicationProp[] }> = ({
     <table className="table-auto border-collapse border border-slate-400 shadow-xl w-full text-sm md:text-base">
       <thead className=" bg-gray-100 text-left">
         <tr>
-          <th className="hidden lg:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
+          <th className="relative hidden lg:table-cell align-top border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
             Company
+            <div className="inline-block ml-1 absolute right-2 top-3.5">
+              <ChevronUpIcon className="w-[13px] h-[13px] cursor-pointer mb-1" />
+              <ChevronDownIcon className="w-[13px] h-[13px] cursor-pointer" />
+            </div>
           </th>
-          <th className="border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
+          <th className="relative border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
             Position
+            <div className="hidden md:inline-block ml-1 absolute right-2 top-3.5">
+              <ChevronUpIcon className="w-[13px] h-[13px] cursor-pointer mb-1" />
+              <ChevronDownIcon className="w-[13px] h-[13px] cursor-pointer" />
+            </div>
           </th>
-          <th className="hidden sm:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
+          <th className="relative hidden sm:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
             Status
+            <div className="inline-block ml-1 absolute right-2 top-3.5">
+              <ChevronUpIcon className="w-[13px] h-[13px] cursor-pointer mb-1" />
+              <ChevronDownIcon className="w-[13px] h-[13px] cursor-pointer" />
+            </div>
           </th>
-          <th className="hidden lg:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
+          <th className="relative hidden lg:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
             Date
+            <div className="inline-block ml-1 absolute right-2 top-3.5">
+              <ChevronUpIcon className="w-[13px] h-[13px] cursor-pointer mb-1" />
+              <ChevronDownIcon className="w-[13px] h-[13px] cursor-pointer" />
+            </div>
           </th>
           <th className="border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4">
             Note
