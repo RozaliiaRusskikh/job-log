820f80beedebf2d87d7e9056dfdec507101e15d5
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-06 15:55:19 -0800

    Delete link to homepage

--- app/ui/job-logs-logo.tsx
+++ app/ui/job-logs-logo.tsx
@@ -1,6 +1,5 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { roboto } from "@/app/ui/fonts";
-import Link from "next/link";
 
 interface LogoProps {
   className?: string;
@@ -8,13 +7,12 @@ interface LogoProps {
 
 const JobLogsLogo: React.FC<LogoProps> = ({ className }) => {
   return (
-    <Link
-      href="/"
+    <div
       className={`${roboto.className} flex flex-row items-center text-white ${className}`}
     >
       <MagnifyingGlassIcon className="h-10 w-10 mr-1 rotate-[5deg] text-emerald-500" />
       <p className="text-[37px] md:text-[50px]">JobLogs</p>
-    </Link>
+    </div>
   );
 };
 
