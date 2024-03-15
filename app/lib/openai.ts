deb6847a0a8e6a6ed752028a3bc2435ebef73d15
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-15 13:00:32 -0700

    Jenerate embedding

--- app/lib/openai.ts
+++ app/lib/openai.ts
@@ -10,4 +10,15 @@ const openai = new OpenAI({ apiKey });
 
 export default openai;
 
-export async function getEmbedding(text: string) {}
+export async function getEmbedding(text: string) {
+  const response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: text,
+  });
+  const embedding = response.data[0].embedding;
+
+  if (!embedding) {
+    throw Error("Error generationg embedding.");
+  }
+  return embedding;
+}
