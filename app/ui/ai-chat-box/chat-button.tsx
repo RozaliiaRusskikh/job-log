997f44c02d592cb2f5d87a741bac0bb114242330
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-19 16:41:15 -0700

    Minor style fixes

--- app/ui/ai-chat-box/chat-button.tsx
+++ app/ui/ai-chat-box/chat-button.tsx
@@ -7,10 +7,10 @@ export function ChatButton() {
   return (
     <>
       <button
-        className="flex items-center group text-white font-bold md:border-2 md:rounded-full px-1 md:px-2 md:py-3 border-[#848081] hover:border-emerald-400 transition-colors"
+        className="flex md:bg-gray-900 shadow-lg items-center group text-white font-bold md:border-2 md:rounded-full px-1 md:px-2 md:py-3 border-[#848081] hover:border-emerald-400 transition-colors"
         onClick={() => setChatBoxOpen(true)}
       >
-        <span className="hidden md:inline group-hover:text-emerald-100 transition-colors">
+        <span className="hidden md:inline group-hover:text-emerald-300 transition-colors">
           AI Chat
         </span>
         <ChatBubbleOvalLeftEllipsisIcon className=" w-[30px] h-[30px] md:ml-1 inline text-emerald-500 md:text-white group-hover:text-white md:group-hover:text-slate-200 transition-all md:group-hover:animate-pulse" />
