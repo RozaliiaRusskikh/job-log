130053cd74db27bcca8b1097138237e6b50c3a34
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-20 16:19:24 -0700

    Add debouncing

--- app/ui/job-application-table/search.tsx
+++ app/ui/job-application-table/search.tsx
@@ -2,13 +2,18 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, ChangeEvent } from "react";
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -17,7 +22,13 @@ const Search = () => {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 500);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    handleSearch(value);
+  };
 
   return (
     <div className="relative flex flex-1 mb-3 xl:w-2/3">
@@ -25,15 +36,13 @@ const Search = () => {
         Search
       </label>
       <input
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
+        onChange={handleChange}
         className="truncate block w-full rounded-md border border-slate-300 py-[9px] pl-8 md:pl-10 text-sm md:text-[14.5px] outline-2 placeholder:text-gray-500 outline-emerald-500"
         type="text"
         name="search"
         id="search"
         placeholder="Search for a company name or a job title"
-        defaultValue={searchParams.get("query")?.toString()}
+        value={searchTerm}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
     </div>
