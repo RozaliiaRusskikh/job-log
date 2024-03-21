a875d32661bc7063a6f7d6070ab3613072b7fa68
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-20 17:19:34 -0700

    Save changes

--- app/ui/job-application-table/table.tsx
+++ app/ui/job-application-table/table.tsx
@@ -13,7 +13,7 @@ export default async function Table({ query }: { query?: string }) {
       <p className="text-center italic pt-3 text-gray-700">
         Sorry, we couldn't find any job applications related to your entered
         query. Please try another search
-        <MagnifyingGlassMinusIcon className="text-emerald-600 inline w-[30px] h-[30px] ml-1" />
+        <MagnifyingGlassMinusIcon className="text-emerald-600 sm:inline w-[30px] h-[30px] ml-1 relative left-1/2 sm:static" />
       </p>
     );
   }
@@ -24,17 +24,11 @@ export default async function Table({ query }: { query?: string }) {
         <tr>
           <th className="relative hidden lg:table-cell align-top border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4 lg:px-6">
             Company
-            <div className="inline-block ml-1 absolute right-2 top-3.5 lg:top-1/4">
-              <ChevronUpIcon className="w-[14px] h-[14px] cursor-pointer text-emerald-800" />
-              <ChevronDownIcon className="w-[14px] h-[14px] cursor-pointer  text-emerald-800" />
-            </div>
+            <div className="inline-block ml-1 absolute right-2 top-3.5 lg:top-1/4"></div>
           </th>
           <th className="relative border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4 lg:px-6">
             Position
-            <div className="hidden lg:inline-block ml-1 absolute right-2 top-3.5 lg:top-1/4">
-              <ChevronUpIcon className="w-[14px] h-[14px] cursor-pointer text-emerald-800" />
-              <ChevronDownIcon className="w-[14px] h-[14px] cursor-pointer text-emerald-800" />
-            </div>
+            <div className="hidden lg:inline-block ml-1 absolute right-2 top-3.5 lg:top-1/4"></div>
           </th>
           <th className="relative hidden sm:table-cell border-collapse border border-slate-300 px-2 py-2 md:px-4 md:py-4 lg:px-6">
             Status
