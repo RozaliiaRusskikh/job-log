6732bb46c08085b0afbebca03f57b0f883b4aecc
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-03-22 15:24:39 -0700

    Minor style fixes

--- app/ui/job-application-table/unauthorized.tsx
+++ app/ui/job-application-table/unauthorized.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Unauthorized = () => {
   return (
-    <main className="mt-20 flex items-start justify-center h-screen bg-gray-100">
-      <div className="flex gap-3 justify-center items-center flex-col md:flex-row">
+    <main className="mt-20 flex items-start md:items-center justify-center h-screen bg-gray-100">
+      <div className="flex gap-2 justify-center items-center flex-col">
         <FaceFrownIcon className="text-emerald-600" width={40} height={40} />
         <h2 className="text-primary text-lg text-center">
-          Access Denied. Please{" "}
+          <span className="font-semibold block">Access Denied.</span> Please{" "}
           <Link
             href="/"
             className="cursor-pointer decoration-2 decoration-emerald-500 underline hover:text-emerald-600 transition-colors"
